32e17c199c8c087b4229c6e03d1d254b5d747573
Tesfa Bizuneh <41780689+tbizuneh@user.noreply.github.com>
2019-02-09 11:45:23 -0500

    second commit

--- shoppingkart/src/components/layout/Landing.js
+++ shoppingkart/src/components/layout/Landing.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
+
 class Landing extends Component {
   render() {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+            <p className="flow-text green-text text-darken-1">
+              {/* <h1>ShopSmart </h1> */}
             </p>
             <br />
             <a href
